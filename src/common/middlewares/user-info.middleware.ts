332c9523d40a55f8de6b016212b1dbbddc0cd398
akshat <akshat.sandhaliya@zangula.com>
2023-05-01 18:46:24 +0530

    use res locals

--- src/common/middlewares/user-info.middleware.ts
+++ src/common/middlewares/user-info.middleware.ts
@@ -1,19 +1,17 @@
-import { Inject, NestMiddleware } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
+import { NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
-import { User } from '../../users/entities/user.entity';
 import { UsersService } from "src/users/users.service";
 
 export class UserInfoMiddleware implements NestMiddleware {
     constructor(private readonly usersService: UsersService) {}
 
     async use(req: Request, res: Response, next: (error?: NextFunction) => void) {
-        if (req.user) {
-            const { id } = req.user;
+        if (res.locals.user) {
+            const id = res.locals.user?.id;
             const userInfo = await this.usersService.findOne({
                 id: +id,
             });
-            Object.assign(req.user, userInfo);
+            Object.assign(res.locals.user, userInfo);
         }   
         next();
     }
